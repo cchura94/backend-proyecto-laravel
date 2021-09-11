361cfb497cb9c5bbdf6c33d5e1b67f6f780fc7ae
cchura94 <cchura.cpc@gmail.com>
2021-09-10 22:01:46 -0400

    Models y migraciones

---
+++ database/migrations/2021_09_11_015715_create_producto_sucursal_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoSucursalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto_sucursal', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto_sucursal');
+    }
+}
